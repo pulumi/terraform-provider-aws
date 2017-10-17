105f6dd3297d92c53f3a3a8eaeb14f3d7ff1fa3b
Luke Hoban <luke@pulumi.com>
2017-10-17 14:20:55 -0700

    Add ECR credentials data source
    This enables getting ECR login credentials into a Terraform script to use in provisioners or other places where an ECR repository must be pushed or pulled as part of a deployment.

---
+++ aws/data_source_aws_ecr_credentials.go
@@ -0,0 +1,67 @@
+package aws
+
+import (
+	"log"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/ecr"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceAwsEcrCredentials() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAwsEcrCredentialsRead,
+
+		Schema: map[string]*schema.Schema{
+			"registry_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"authorization_token": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"expires_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"proxy_endpoint": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceAwsEcrCredentialsRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).ecrconn
+
+	registryID := d.Get("registry_id").(string)
+	log.Printf("[DEBUG] Reading ECR repository credentials %s", registryID)
+
+	out, err := conn.GetAuthorizationToken(&ecr.GetAuthorizationTokenInput{
+		RegistryIds: []*string{aws.String(registryID)},
+	})
+
+	if err != nil {
+		if ecrerr, ok := err.(awserr.Error); ok && ecrerr.Code() == "ErrCodeInvalidParameterException" {
+			log.Printf("[WARN] ECR Repository %s not found", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+
+	auth := out.AuthorizationData[0]
+	log.Printf("[DEBUG] Received ECR repository credentials %s", out)
+
+	d.SetId(registryID)
+	d.Set("authorization_token", auth.AuthorizationToken)
+	d.Set("expires_at", auth.ExpiresAt.Format(time.RFC3339))
+	d.Set("proxy_endpoint", auth.ProxyEndpoint)
+
+	return nil
+}
