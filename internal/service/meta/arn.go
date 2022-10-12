10b16ea5e764e290ea437df39eea6b2b15527728
Daniel Bradley <daniel@pulumi.com>
2022-10-12 14:20:08 +0100

    Strip new framework from ARN

--- internal/service/meta/arn.go
+++ internal/service/meta/arn.go
@@ -3,12 +3,7 @@ package meta
 import (
 	"context"
 	"fmt"
-
 	"github.com/aws/aws-sdk-go-v2/aws/arn"
-	"github.com/hashicorp/terraform-plugin-framework/attr"
-	"github.com/hashicorp/terraform-plugin-framework/attr/xattr"
-	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
 )
 
@@ -18,37 +13,8 @@ const (
 	ARNType arnType = iota
 )
 
-var (
-	_ xattr.TypeWithValidate = ARNType
-)
-
 func (t arnType) TerraformType(_ context.Context) tftypes.Type {
 	return tftypes.String
-}
-
-func (t arnType) ValueFromTerraform(_ context.Context, in tftypes.Value) (attr.Value, error) {
-	if !in.IsKnown() {
-		return ARN{Unknown: true}, nil
-	}
-	if in.IsNull() {
-		return ARN{Null: true}, nil
-	}
-	var s string
-	err := in.As(&s)
-	if err != nil {
-		return nil, err
-	}
-	a, err := arn.Parse(s)
-	if err != nil {
-		return nil, err
-	}
-	return ARN{Value: a}, nil
-}
-
-// Equal returns true if `o` is also an ARNType.
-func (t arnType) Equal(o attr.Type) bool {
-	_, ok := o.(arnType)
-	return ok
 }
 
 // ApplyTerraform5AttributePathStep applies the given AttributePathStep to the
@@ -62,48 +28,6 @@ func (t arnType) String() string {
 	return "types.ARNType"
 }
 
-// Validate implements type validation.
-func (t arnType) Validate(ctx context.Context, in tftypes.Value, path path.Path) diag.Diagnostics {
-	var diags diag.Diagnostics
-
-	if !in.Type().Is(tftypes.String) {
-		diags.AddAttributeError(
-			path,
-			"ARN Type Validation Error",
-			"An unexpected error was encountered trying to validate an attribute value. This is always an error in the provider. Please report the following to the provider developer:\n\n"+
-				fmt.Sprintf("Expected String value, received %T with value: %v", in, in),
-		)
-		return diags
-	}
-
-	if !in.IsKnown() || in.IsNull() {
-		return diags
-	}
-
-	var value string
-	err := in.As(&value)
-	if err != nil {
-		diags.AddAttributeError(
-			path,
-			"ARN Type Validation Error",
-			"An unexpected error was encountered trying to validate an attribute value. This is always an error in the provider. Please report the following to the provider developer:\n\n"+
-				fmt.Sprintf("Cannot convert value to arn.ARN: %s", err),
-		)
-		return diags
-	}
-
-	if !arn.IsARN(value) {
-		diags.AddAttributeError(
-			path,
-			"ARN Type Validation Error",
-			fmt.Sprintf("Value %q cannot be parsed as an ARN.", value),
-		)
-		return diags
-	}
-
-	return diags
-}
-
 func (t arnType) Description() string {
 	return `An Amazon Resource Name.`
 }
@@ -112,10 +36,6 @@ type ARN struct {
 	Unknown bool
 	Null    bool
 	Value   arn.ARN
-}
-
-func (a ARN) Type(_ context.Context) attr.Type {
-	return ARNType
 }
 
 func (a ARN) ToTerraformValue(ctx context.Context) (tftypes.Value, error) {
@@ -127,21 +47,6 @@ func (a ARN) ToTerraformValue(ctx context.Context) (tftypes.Value, error) {
 		return tftypes.NewValue(t, tftypes.UnknownValue), nil
 	}
 	return tftypes.NewValue(t, a.Value.String()), nil
-}
-
-// Equal returns true if `other` is a *ARN and has the same value as `a`.
-func (a ARN) Equal(other attr.Value) bool {
-	o, ok := other.(ARN)
-	if !ok {
-		return false
-	}
-	if a.Unknown != o.Unknown {
-		return false
-	}
-	if a.Null != o.Null {
-		return false
-	}
-	return a.Value == o.Value
 }
 
 // IsNull returns true if the Value is not set, or is explicitly set to null.
@@ -162,13 +67,5 @@ func (a ARN) IsUnknown() bool {
 // logging and error reporting, as they are not protected by
 // compatibility guarantees within the framework.
 func (a ARN) String() string {
-	if a.IsUnknown() {
-		return attr.UnknownValueString
-	}
-
-	if a.IsNull() {
-		return attr.NullValueString
-	}
-
 	return a.Value.String()
 }
