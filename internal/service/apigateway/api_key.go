94e315943a2b8f624cdc661cb2cafd40f1acb657
Daniel Bradley <daniel@pulumi.com>
2023-02-28 15:19:24 +0000

    Change default descriptions to "Managed by Pulumi"

--- internal/service/apigateway/api_key.go
+++ internal/service/apigateway/api_key.go
@@ -47,7 +47,7 @@ func ResourceAPIKey() *schema.Resource {
 			"description": {
 				Type:     schema.TypeString,
 				Optional: true,
-				Default:  "Managed by Terraform",
+				Default:  "Managed by Pulumi",
 			},
 
 			"enabled": {
